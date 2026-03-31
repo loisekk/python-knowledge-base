47be1c575b5c7527052e565f62e9e3c6efb69778
YASH BRAHMANKAR <yashbrahmankar95@gmail.com>
2026-04-01 02:31:27 +0530

    Uncomment code for product and triangle calculations

--- first_test.py
+++ first_test.py
@@ -1,39 +1,39 @@
-# n  = int(input("tell your product amount :-"))
+n  = int(input("tell your product amount :-"))
 
-# if n >5000 :
-#     print("you got 25% discount ")
+if n >5000 :
+    print("you got 25% discount ")
 
-# elif n >= 3000 or n <= 5000:
-#     print("you got 15 % discount")
-# elif n >= 1000 or n <=3000 :
-#     print("you got 10% discount")
-# else:
-#     print("there is no discount")
+elif n >= 3000 or n <= 5000:
+    print("you got 15 % discount")
+elif n >= 1000 or n <=3000 :
+    print("you got 10% discount")
+else:
+    print("there is no discount")
 
 
 ''' take three as in input representing triangle'''
 
-# a = int(input("tell your first side's for triangle :-"))
-# b = int(input("tell your second side's for triangle :-"))d
-# c = int(input("tell your third side's for triangle :-"))
+a = int(input("tell your first side's for triangle :-"))
+b = int(input("tell your second side's for triangle :-"))d
+c = int(input("tell your third side's for triangle :-"))
 
-# if a== b or b ==c or a==c :
-#     print("Equilateral Triangle")
+if a== b or b ==c or a==c :
+    print("Equilateral Triangle")
 
-# elif a==b or b ==c or a ==c :
-#     print("Isoscaler Triangle")
+elif a==b or b ==c or a ==c :
+    print("Isoscaler Triangle")
 
-# elif a==b or b==c or a==c :
-#     print("Scalene Triangle")
+elif a==b or b==c or a==c :
+    print("Scalene Triangle")
 
 
-# n = int(input("tell your number:-"))
+n = int(input("tell your number:-"))
 
-# for i in range(1,n+1):
-#     if i % 2 == 0  :
-#      print(i)
-#     elif(i % 2 != 0):
-#      print(i) nbvn
+for i in range(1,n+1):
+    if i % 2 == 0  :
+     print(i)
+    elif(i % 2 != 0):
+     print(i) nbvn
 
 
 ''' URL = UNIFORM RESOURCE LOCATOR ''' 
@@ -48,29 +48,30 @@
  
 
 
-# n = int(input("tell your number:-"))
-# for i in range (1,n):
-#   if i > 1 or n % i ==0:
-#     print(f" its a prime number and it's divisor number  is {n}")
-#     break
+n = int(input("tell your number:-"))
+for i in range (1,n):
+  if i > 1 or n % i ==0:
+    print(f" its a prime number and it's divisor number  is {n}")
+    break
 
-#   else:
-#     print("not a prime number")
-#     break
+  else:
+    print("not a prime number")
+    break
 
-# n = int(input("enter a number :-"))
+n = int(input("enter a number :-"))
 
-# factor = 0
-# for i in range (1,n+1):
-#   if n %i == 0 :
-#     factor +=1
+factor = 0
+for i in range (1,n+1):
+ 
+  if n %i == 0 :
+    factor +=1
     
-#   if factor >2 :
-#     print("Not a prime number ")
-#     break
-#   else:
-#     print("prime number ")
-#     break
+  if factor >2 :
+    print("Not a prime number ")
+    break
+  else:
+    print("prime number ")
+    break
 
 n = int(input("Enter a prime :-"))
 factor = 0 
